f0fccfa24f8f66f1502a3e50a9074a16dcd40647
smintank <assbestmail@gmail.com>
2024-12-27 22:50:38 +0400

    Add github actions

--- main.py
+++ main.py
@@ -12,17 +12,11 @@ logger = logging.getLogger(__name__)
 def main():
     logger.info("App started successfully")
     driver = get_webdriver()
-    sleep_time = config.CLICK_FREQUENCY_IN_MIN * 3600
-
     try:
         load_page(driver, config.SIGNIN_PAGE)
         sign_in(driver)
-
-        while True:
-            load_page(driver, config.WORKING_PAGE)
-            save_plays(driver)
-            logger.info("Sleep for %s m", config.CLICK_FREQUENCY_IN_MIN)
-            time.sleep(sleep_time)
+        load_page(driver, config.WORKING_PAGE)
+        save_plays(driver)
     except Exception as e:
         logger.error("An error occurred: %s", e)
     finally:
