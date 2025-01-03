69505efa6c8c3f50ff25f14f43b967d1387cd370
smintank <assbestmail@gmail.com>
2025-01-03 20:53:39 +0400

    Hotfix

--- utils.py
+++ utils.py
@@ -44,7 +44,7 @@ def save_plays(driver: WebDriver) -> None:
         try:
             is_duplicate_badge = card.find_elements(By.XPATH, f"//p[text()='{config.DUPLICATE_TEXT_ALERT}']")
             card_id_element = card.find_element(By.CSS_SELECTOR, config.CARD_ID_SELECTOR)
-            logger.info(card_id_element)
+            logger.info(str(card_id_element))
             card_id = card_id_element.get_attribute(config.CARD_ID_ATTR)
             logger.info(card_id)
 
